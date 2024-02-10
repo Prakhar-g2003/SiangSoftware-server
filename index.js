a56c7f89a977da9dce2eccfa6f8e5d75faed67d0
Prakhar-g2003 <prakharpg360@gmail.com>
2024-02-10 21:00:09 +0530

    cors

--- index.js
+++ index.js
@@ -8,39 +8,13 @@ const feed = require('./api/feed');
 const port = 3001;
 const cors = require('cors');
 const http = require('http');
-const socketIo = require('socket.io');
-const rateLimit = require('express-rate-limit');
 const app = express();
+
 app.use(cors());
 
 
-const server = http.createServer(app);
-const io = socketIo(server);
-
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // Event listener for incoming messages
-  socket.on('chat message', (msg) => {
-    // Implement message filtering logic here
-    if (msgContainsSpam(msg)) {
-      // Notify the user of spam detection
-      socket.emit('spam detected', 'Your message was detected as spam.');
-      return;
-    }
-
-    // Broadcast the message to all connected clients
-    io.emit('chat message', msg);
-  });
-
-  // Event listener for disconnections
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
-
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://adittya-gupta.github.io");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
